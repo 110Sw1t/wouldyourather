44985407820721eb7e84f2d11d5d4fdc4e895723
AFoxWorks <dev.fassem@gmail.com>
2021-07-19 16:15:01 +0200

    Refactor session handling

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,11 @@
 import { Component } from 'react';
 import { connect } from 'react-redux';
 import { Switch } from 'react-router-dom';
-import history from '../routes/history';
 import LoginPage from '../routes/LoginPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 class App extends Component {
-
-
-  componentDidMount() {
-    
-
-
-
-  }
-
-  componentDidUpdate(prevProps) {
-    // If user logged in then push new page
-    if (!prevProps.loggedInUser || !Object.keys(prevProps.loggedInUser).length) {
-      if (this.props.loggedInUser && Object.keys(this.props.loggedInUser).length) {
-        history.push("/");
-      }
-    }
-  }
 
   render() {
     return (
--- src/middleware/authorizer.js
+++ src/middleware/authorizer.js
@@ -1,18 +1,27 @@
-import { LOGIN } from '../actions/session';
+import { LOGIN, LOGOUT } from '../actions/session';
+import history from '../routes/history';
 
 const authorizer = (store) => (next) => (action) => {
 
-   if (action.type === LOGIN) {
-      const { users } = store.getState();
-      if (!users || Object.keys(users).length === 0 || !action.loggedInUser || !action.loggedInUser.id || !users[action.loggedInUser.id]) {
-         alert("Invalid User");
-      } else {
+   switch (action.type) {
+      case LOGIN:
+         const { users } = store.getState();
+         if (!users || Object.keys(users).length === 0 || !action.loggedInUser || !action.loggedInUser.id || !users[action.loggedInUser.id]) {
+            alert("Invalid User");
+         } else {
+            const res = next(action);
+            history.push("/");
+            return res;
+         }
+         break;
+      case LOGOUT:
+         next(action);
+         history.push("/login");
+         break;
+      default:
          return next(action);
-      }
-
-   } else {
-      return next(action);
    }
+
 
 }
 
